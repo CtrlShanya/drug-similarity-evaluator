f98ba7af9e638e9d462587c23a8e545dddd6d545
Shanya <shanyasinghal.becse17@pec.edu.in>
2025-01-17 22:40:20 +0100

    basic app setup

---
+++ src/drug_similarity_evaluator/__main__.py
@@ -0,0 +1,26 @@
+import typing as t
+import typer
+
+app = typer.Typer()
+
+
+class Molecule:
+    name: str
+    smiles: str
+    fingerprint: t.Any
+
+    def calculate_similarity(self, other_molecule: "Molecule") -> None: ...
+
+
+class DrugLibrary:
+    molecules_list: t.List[Molecule]
+
+    def find_top_similar(self, top_n: int) -> None: ...
+
+
+@app.command()
+def drug_similarity_evaluator() -> None: ...
+
+
+if __name__ == "__main__":
+    typer.run(app)
