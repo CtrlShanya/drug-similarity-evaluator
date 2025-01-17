038b5a2101bc6950cd23409efabb93964c819d7c
Shanya <shanyasinghal.becse17@pec.edu.in>
2025-01-17 23:47:27 +0100

    adding method implementations for Molecule

--- src/drug_similarity_evaluator/__main__.py
+++ src/drug_similarity_evaluator/__main__.py
@@ -1,6 +1,9 @@
 import typing as t
 
 import typer
+from rdkit import Chem
+from rdkit.Chem import AllChem
+from rdkit.DataStructs.cDataStructs import TanimotoSimilarity
 
 app = typer.Typer()
 
@@ -10,7 +13,20 @@ class Molecule:
     smiles: str
     fingerprint: t.Any
 
-    def calculate_similarity(self, other_molecule: "Molecule") -> None: ...
+    def __int__(self, name: str, smiles: str) -> None:
+        self.name = name
+        self.smiles = smiles
+        molecule = Chem.MolFromSmiles(smiles)
+        self.fingerprint = (
+            AllChem.GetMorganFingerprintAsBitVect(molecule, radius=2)
+            if molecule
+            else None
+        )
+
+    def calculate_similarity(self, other_molecule: "Molecule") -> float:
+        if not self.fingerprint or not other_molecule.fingerprint:
+            return 0.0
+        return TanimotoSimilarity(self.fingerprint, other_molecule.fingerprint)
 
 
 class DrugLibrary:
